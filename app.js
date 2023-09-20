d16a050d4f0b32e26adfe001a3c6414f76bee6b7
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-09-20 05:53:58 +0000

    [update]mouseover,mouseover,click

--- app.js
+++ app.js
@@ -33,12 +33,41 @@
 
 
 // 確認問題
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'height': '100px',
+//       'width': '200px'
+//     }).slideUp();
+//   });
+// });
+
+// マウスオーバー
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').css({'background-color': '#0000FF'});
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').css({'background-color': '#FF0000'});
+//   });
+// });
+
+// $(function(){
+//   $('.box1').mouseover(function(){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function(){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// マウスクリック
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'height': '100px',
-      'width': '200px'
-    }).slideUp();
+  $('.box1').on('click', function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
